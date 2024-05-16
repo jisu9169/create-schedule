1155bb471f77be5e51d444956b7637eab9bac49e
KIM <wltn9169@naver.com>
2024-05-16 19:20:26 +0900

    git init

---
+++ src/main/java/org/sparta/createschedule/CreateScheduleApplication.java
@@ -0,0 +1,13 @@
+package org.sparta.createschedule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreateScheduleApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CreateScheduleApplication.class, args);
+  }
+
+}
---
+++ src/test/java/org/sparta/createschedule/CreateScheduleApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sparta.createschedule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreateScheduleApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
