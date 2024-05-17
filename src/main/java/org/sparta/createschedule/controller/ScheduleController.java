677d3880b24f693953e1f4d563389cfe380ccb4d
KIM <wltn9169@naver.com>
2024-05-17 13:37:27 +0900

    7단계 Swagger @Operation 추가

--- src/main/java/org/sparta/createschedule/controller/ScheduleController.java
+++ src/main/java/org/sparta/createschedule/controller/ScheduleController.java
@@ -1,6 +1,7 @@
 package org.sparta.createschedule.controller;
 
 
+import io.swagger.v3.oas.annotations.Operation;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.sparta.createschedule.dto.ScheduleRequestDto;
@@ -25,27 +26,32 @@ public class ScheduleController {
   private final ScheduleService scheduleService;
 
   @PostMapping("/create")
+  @Operation(summary = "일정 생성", description = "일정 생성 API")
   public ResponseEntity<ScheduleResponseDto> CreateSchedule(
       @RequestBody ScheduleRequestDto scheduleRequestDto) {
     return ResponseEntity.ok().body(scheduleService.save(scheduleRequestDto));
   }
 
   @GetMapping("/get/{id}")
+  @Operation(summary = "일정 단건 조회", description = "일정 단건 조회 API")
   public ResponseEntity<ScheduleResponseDto> getSchedule(@PathVariable Long id){
     return ResponseEntity.ok().body(scheduleService.getSchedule(id));
   }
 
   @GetMapping("/getall")
+  @Operation(summary = "일정 모두 조회", description = "일정 모두 조회 API")
   public ResponseEntity<List<ScheduleResponseDto>> getAllSchedule() {
     return ResponseEntity.ok().body(scheduleService.getAllSchedule());
   }
 
   @PatchMapping("/patch")
+  @Operation(summary = "일정 수정", description = "일정 수정 API")
   public ResponseEntity<ScheduleResponseDto> UpdateSchedule(@RequestBody ScheduleRequestDto scheduleRequestDto) {
     return ResponseEntity.ok().body(scheduleService.updateSchedule(scheduleRequestDto));
   }
 
   @DeleteMapping("/delete")
+  @Operation(summary = "일정 삭제", description = "일정 삭제 API")
   public ResponseEntity<ScheduleResponseDto> DeleteSchedule(@RequestBody ScheduleRequestDto scheduleRequestDto) {
     return ResponseEntity.ok().body(scheduleService.deleteSchedule(scheduleRequestDto));
   }
